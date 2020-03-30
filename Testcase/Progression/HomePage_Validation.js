331fbb41d2c4dcbe7b8add652726b0b22899a476
Dina Cherepanova <dCherepanova@eighthythreecreative.com>
2020-03-30 16:56:39 -0500

    Initial test commit

--- Testcase/Progression/HomePage_Validation.js
+++ Testcase/Progression/HomePage_Validation.js
@@ -22,7 +22,31 @@ describe('Clicking on Home Page buttons', function () {
 	//-----------------------------------------First Navigation Tabs TCs--------------------------//
 
 	it('Test', async function () {
-		
+		await console.log("Test mesage")
+	});
+
+	it('Test', async function () {
+		await console.log("Test mesage")
+	});
+
+	it('Test', async function () {
+		await console.log("Test mesage")
+	});
+
+	it('Test', async function () {
+		await console.log("Test mesage")
+	});
+
+	it('Test', async function () {
+		await console.log("Test mesage")
+	});
+
+	it('Test', async function () {
+		await console.log("Test mesage")
+	});
+
+	it('Test', async function () {
+		await console.log("Test mesage")
 	});
 
 
