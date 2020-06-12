248518f8cf1f27e70189982d4b0b326a6118ec18
Dina Cherepanova <dCherepanova@eighthythreecreative.com>
2020-06-12 17:12:23 -0500

    final

--- os.conf.js
+++ os.conf.js
@@ -8,8 +8,8 @@ var basePath = __dirname;
 var path = require('path');
 var downloadsPath = path.resolve(__dirname, './Downloads');
 
-//var specArray = ['Testcase/Progression/HomePage_Validation.js', 'Testcase/Progression/CounterDay_Validation.js']
-var specArray = ['Testcase/Progression/HomePage_Validation.js']
+var specArray = ['Testcase/Progression/HomePage_Validation.js', 'Testcase/Progression/CounterDay_Validation.js']
+//var specArray = ['Testcase/Progression/HomePage_Validation.js']
 // var specArray = ['Testcase/Progression/CounterDay_Validation.js']
 
 exports.config = {
