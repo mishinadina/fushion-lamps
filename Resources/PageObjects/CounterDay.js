83822e52d78aba4ad262bdef064b78e26610263f
Dina Cherepanova <dCherepanova@eighthythreecreative.com>
2020-06-15 15:33:31 -0500

    test4

--- Resources/PageObjects/CounterDay.js
+++ Resources/PageObjects/CounterDay.js
@@ -10,7 +10,7 @@ var CounterDay_Form = function () {
     var n;
     var y;
 
-    
+
     var Lunch = by.xpath("//*[@id='Collection']/div/div/div/ul/li[" + n + "]/div/div[5]/a")
     var ProductList = by.xpath('//*[@id="ProductSection-product-template"]/div/div/div[2]')
 
@@ -44,6 +44,76 @@ var CounterDay_Form = function () {
         var Breakfast = by.xpath("//*[@id='Collection']/div/div/div/ul/li[" + n + "]/div/div[3]/a")
         await GUILib.clickObject(Breakfast)
         await GUILib.waitforElement(QuantityName)
+    }
+
+    this.clickBBQ = async function (n) {
+        var BBQ = by.xpath("//*[@id='Collection']/div/div/div/ul/li[" + n + "]/div/div[4]/a")
+        await GUILib.clickObject(BBQ)
+        await GUILib.waitforElement(QuantityName)
+    }
+
+    this.clickLunch = async function (n) {
+        var BBQ = by.xpath("//*[@id='Collection']/div/div/div/ul/li[" + n + "]/div/div[4]/a")
+        await GUILib.clickObject(BBQ)
+        await GUILib.waitforElement(QuantityName)
+    }
+
+    this.checkProductsPage = async function () {
+        var Arr = [];
+        await browser.getCurrentUrl().then(async function (url) {
+            await element.all(QuantityLines).count().then(async function (lines) {
+                await console.log(lines)
+                for (y = 1; y <= lines; y++) {
+                    var Quantity = by.xpath('//*[@id="counter-day-form"]/div[1]/div/table/tbody/tr[' + y + ']/td[4]/input')
+                    var Product = by.xpath("//*[@id='counter-day-form']/div[1]/div/table/tbody/tr[" + y + "]/td[2]/a")
+                    var ProductDescription = by.xpath('//*[@id="counter-day-form"]/div[1]/div/table/tbody/tr[' + y + ']/td[2]/a')
+                    await GUILib.getText(ProductDescription).then(async function (text) {
+                        await GUILib.getAttribute(Quantity, 'value').then(async function (count) {
+                            await console.log(count)
+                            if (count > 0) {
+                                await GUILib.clickObject(Product)
+                                await browser.sleep(500)
+                                let windowHandles = await browser.getAllWindowHandles();
+                                await browser.switchTo().window(windowHandles[1])
+                                await GUILib.waitforElement(Logo)
+                                await element.all(Error404).count().then(async function (Error404) {
+                                    await console.log(Error404)
+                                    if (Error404 > 0) {
+                                        await Arr.push(text);
+                                    }
+                                })
+                                await browser.close()
+                                await browser.switchTo().window(windowHandles[0])
+                                await browser.get(url)
+                                await GUILib.waitforElement(Quantity)
+                            }
+                        })
+                    })
+                }
+            })
+        })
+        await console.log(Arr)
+        expect(Arr.length).toBe(0)
+    }
+
+    this.fillBundleInfoPastDate = async function () {
+        await GUILib.typeValue(Vendors, 'Test')
+        await GUILib.selectFromDropdown(StartHourOptions, '1')
+        await GUILib.selectFromDropdown(EndHourOptions, '1')
+        await GUILib.selectFromDropdown(StartMinuteOptions, '1')
+        await GUILib.selectFromDropdown(EndMinuteOptions, '1')
+        await GUILib.clickObject(Calendar)
+        try {
+            await GUILib.clickObject(DateCalendar)
+            await GUILib.clickObject(AddToCartBtn)
+            await GUILib.waitforElement(ViewCartBtn)
+            await GUILib.clickObject(ViewCartBtn)
+            boolean = false
+        }
+        catch {
+            boolean = true
+        }
+        expect(boolean).toBe(true)
     }
 
     
