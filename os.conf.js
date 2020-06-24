db26e37a4177cd4110be6b3bd8d81acf018bcb9d
Dina Cherepanova <dCherepanova@eighthythreecreative.com>
2020-06-24 17:13:54 -0500

    without store tc

--- os.conf.js
+++ os.conf.js
@@ -8,7 +8,8 @@ var basePath = __dirname;
 var path = require('path');
 var downloadsPath = path.resolve(__dirname, './Downloads');
 
-var specArray = ['Testcase/Progression/HomePage_Validation.js', 'Testcase/Progression/CounterDay_Validation.js','Testcase/Progression/Store_Validation.js','Testcase/Progression/Branch_Validation.js','Testcase/Progression/BusinessCard_Validation.js']
+//var specArray = ['Testcase/Progression/HomePage_Validation.js', 'Testcase/Progression/CounterDay_Validation.js','Testcase/Progression/Store_Validation.js','Testcase/Progression/Branch_Validation.js','Testcase/Progression/BusinessCard_Validation.js']
+var specArray = ['Testcase/Progression/HomePage_Validation.js', 'Testcase/Progression/CounterDay_Validation.js','Testcase/Progression/Branch_Validation.js','Testcase/Progression/BusinessCard_Validation.js']
 //var specArray = ['Testcase/Progression/HomePage_Validation.js']
 //var specArray = ['Testcase/Progression/CounterDay_Validation.js']
 //var specArray = ['Testcase/Progression/Store_Validation.js']
